8dde785410a7f7d554ae68cedd020c286b292883
Evelina Kisyova <51174882+EvelinaKisiova@users.noreply.github.com>
2023-01-11 07:22:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const validateInputs = () => {
     if(password2Value === '') {
         setError(password2, 'Please confirm your password');
     } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords doesn't match");
+        setError(password2, "Passwords don't match");
     } else {
         setSuccess(password2);
     }
